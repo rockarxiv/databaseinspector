03e96935d5044c1869011367936d0a43b3839dcb
lyudvig <lyudvig.mkrtchyan@helpsystems.com>
2020-01-26 23:43:44 +0100

    initial commit

---
+++ src/main/java/com/task/databaseinspector/DatabaseinspectorApplication.java
@@ -0,0 +1,13 @@
+package com.task.databaseinspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseinspectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseinspectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/databaseinspector/DatabaseinspectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.databaseinspector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseinspectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
